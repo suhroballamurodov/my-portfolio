d54b173b90bd60e8e07d3d414310ae23cec0797b
Suhrob Allamurodov <asunteam1824@mail.ru>
2024-02-01 17:26:26 +0500

    portfolio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -147,8 +147,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-STATIC_ROOT = os.path.join(BASE_DIR,('staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR,('static'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
